78b624c2231bb7fd86a97af4d5e0ae98cb892130
rkooi88 <66839182+rkooi88@users.noreply.github.com>
2020-10-12 12:21:04 -0500

    removed keys

--- corona.py
+++ corona.py
@@ -1,15 +1,17 @@
 import os
 import tweepy as tw
-import pandas as import pdb; pd
+import numpy as np
+import pandas as pd
+import credentials
 
-search_word = "#coronavirus"
+search_word = "#coronavirus" + " -filter:retweets"
 date_since = "2020-01-01"
 
 tweets = tw.Cursor(api.search,
-              q=search_words,
-              lang="en",
-              since=date_since)
+              q = search_words,
+              lang = "en",
+              since = date_since)
 
 tweet_text = pd.DataFrame(data = time_locs, 
-                    columns=['time', "location"])
-tweet_text
+                    columns=['date', "location"])
+print(tweet_text)
