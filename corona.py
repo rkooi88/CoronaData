d1130cb107b3529f2a986a1d2c003c8b55cf6ca8
rkooi88 <66839182+rkooi88@users.noreply.github.com>
2020-10-11 21:13:28 -0500

    removed keys

--- corona.py
+++ corona.py
@@ -2,11 +2,6 @@ import os
 import tweepy as tw
 import pandas as import pdb; pd
 
-consumer_key = 'DTEvipU4vxQiMBFu3qKobqunL'
-consumer_secret = 'mHlVzlBBY0fuydkcvi9VMT8pVMpIY66eSpxxA5MIadK4l1zmsJ'
-access_token = '1313302374149169154-Eg2fIo3zcc0gBbfazabswDXdZzyW8B'
-access_token_secret = 'FlSKI0SXGrqzdgRAiDXz3EcfGDXZ6paKf0nzPbWgumtay'
-
 search_word = "#coronavirus"
 date_since = "2020-01-01"
 
